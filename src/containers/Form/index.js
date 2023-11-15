258c577607ea1f7a9927b67866a773bdcd2da33a
Smaezz <lea.msi@hotmail.fr>
2023-11-15 14:26:03 +0100

    0errorTest

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,7 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
-        onSuccess()     // Ajouté car manquant
+        onSuccess();     // Ajouté car manquant
       } catch (err) {
         setSending(false);
         onError(err);
